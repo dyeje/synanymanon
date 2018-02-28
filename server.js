452f2a6adc4399eda995df13d5b866ed370814ec
Jeremy Dye <dyeje@mail.gvsu.edu>
2018-02-28 01:27:35 -0500

    Takes sentences

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const moby = require('moby')
+const natural = require('natural');
 const app = express()
 
 app.use(express.json())
@@ -8,9 +9,21 @@ app.use(express.urlencoded({
 }));
 
 var spicy = (req, res) => {
-  res.send(moby.search(req.body.word))
+  tokenizer = new natural.TreebankWordTokenizer()
+  tokens = tokenizer.tokenize(req.body.sentence)
+  token_synonyms = tokens.map((t) => {
+    // only synonym 50% of the time
+    if (Math.random() >= 0.5) {
+      synonyms = moby.search(t)
+      return synonyms[Math.floor(Math.random() * synonyms.length)];
+    } else {
+      return t
+    }
+  })
+  res.send(token_synonyms.join(' '))
+
 }
 
 app.post('/', spicy)
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(2999, () => console.log('Port 2999 Listening'))
